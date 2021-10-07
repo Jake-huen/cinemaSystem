7bcc5ca5b3611e79da0f50cacdac6808e202abdf
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2021-10-07 20:38:21 +0900

    ScreenNameRule

--- src/InputRule.java
+++ src/InputRule.java
@@ -22,7 +22,24 @@ public class InputRule {//입력규칙 정의 (static으로)
 	public static void TimeRule(String time){ //7.4시간입력규칙
 		
 	}
-	public static void ScreenRule(String screen) {//7.5 상영관입력규칙
-		
+	public static String ScreenRule() {//7.5 상영관입력규칙
+		String screen;
+		while(true) {
+			screen=sc.nextLine();
+			String check_screen = screen.trim();
+			if(!screen.equals(check_screen)) {
+				System.out.println("올바르지 않은 입력입니다.");
+			}
+			else if(screen.length()<1 || screen.length()>100) {
+				System.out.println("올바르지 않은 입력입니다.");
+			}
+			else if(screen.charAt(screen.length()-1)!='관'){
+				System.out.println("올바르지 않은 입력입니다.");
+			}
+			else {
+				break;
+			}
+		}
+		return screen;
 	}
 }
