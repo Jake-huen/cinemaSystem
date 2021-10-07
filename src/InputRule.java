9218be959949b3f820dd0eb92041a27d63fb5721
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2021-10-08 00:57:24 +0900

    Update InputRule.java

--- src/InputRule.java
+++ src/InputRule.java
@@ -42,4 +42,38 @@ public class InputRule {//입력규칙 정의 (static으로)
 		}
 		return screen;
 	}
+	
+	public static String SeatRule() { //7.9 예매 좌석 입력 규칙
+        String seat = sc.nextLine();
+        seat = seat.replace(" ", "");
+        seat = seat.trim();
+        seat = seat.toLowerCase();
+        int ascii = seat.charAt(0);
+        if(ascii >= 97 && ascii <= 122) {
+            if(seat.length() > 3 || seat.length() == 1) {
+                return null;
+            }
+            else if(seat.length() == 3) {
+                int a = seat.charAt(1) - '0';
+                int b = seat.charAt(2) - '0';
+                int num = a * 10 + b;
+                if(num >= 1 && num <= 50) return seat;
+                else return null;
+            } else {
+                int num = seat.charAt(1) - '0';
+                if(num >= 1 && num <= 9) return seat;
+                else return null;
+            }
+        } else {
+            return null;
+        }
+    }
+	public static int YesOrNo() { // 7.10 yes / no 입력규칙
+        String yon = sc.next();
+        yon = yon.toLowerCase();
+        if(yon.equals("yes") || yon.equals("y")) return 1; //yes or y --> 1
+        else if(yon.equals("no") || yon.equals("n")) return 0; //no or n --> 0
+        else return -1; //others --> -1
+    }
+
 }
