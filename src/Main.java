e9284053cc48d5909fc737b9d6f6794518c3575b
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2021-10-07 20:18:01 +0900

    MovieTitle_Inputrule
    MTRule() in InputRule

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 	public static void main(String args[]) {
 		//main
 		//초기화면(로그인페이지) 호출
+		System.out.println("영화를 입력해주세요");
+		String movie = InputRule.MTRule();
+		System.out.println(movie);
 	}
-
 }
