3ec35c35a48ac51813f38ee9034dc5c87e582797
rlawngP91 <91997710+rlawngP91@users.noreply.github.com>
2021-10-08 10:22:28 +0900

    7.6날짜입력규칙, 7.7시각입력규칙

--- src/InputRule.java
+++ src/InputRule.java
@@ -165,7 +165,144 @@ public class InputRule {//입력규칙 정의 (static으로)
 		}
 		return screen;
 	}
-	
+
+	public static String DateRule() {// 7.6 날짜입력규칙 _ return 값은 YYYYMMDD형식
+		String checkdate = sc.nextLine();
+		boolean isNumeric = checkdate.chars().allMatch(Character::isDigit);
+		if (isNumeric) {// 문법규칙(1)의 경우
+			if (checkdate.length() == 8) {
+				//매우 올바른형식임_아래 의미규칙만 확인
+			} else if (checkdate.length() == 6) {
+				String s_year = checkdate.substring(0, 2);
+				if(s_year.startsWith("0"))
+					s_year =  s_year.substring(1,2);
+				int year = Integer.parseInt(s_year);
+				if (year >= 50 && year <= 99) {
+					checkdate = "19" + checkdate;
+				}
+				else {
+					checkdate = "20" + checkdate;
+				}
+			} else {
+				return null; // 문법형식에 맞지 않음
+			}
+		} // 문법규칙(2)의 경우
+		else if (checkdate.contains("-") || checkdate.contains("/")) {
+			checkdate = checkdate.replace("/", "-"); // 구분기호 "-로 통일
+			int count = 0;
+			for(int i =0; i<checkdate.length(); i++)
+				if(checkdate.charAt(i)=='-')
+					count++;
+			if(count!=2)	return null;
+			String date[] = checkdate.split("-");
+			if (date[0].length() == 2) {
+				if(date[0].startsWith("0"))
+					date[0] =  date[0].substring(1,2);
+				int year = Integer.parseInt(date[0]);
+				if (year >= 50 && year <= 99)
+					year = 1900 + year;
+				else
+					year = 2000 + year;
+				date[0] = Integer.toString(year);
+			}
+			if (date[1].length() == 1)
+				date[1] = "0" + date[1];
+			if (date[2].length() == 1)
+				date[2] = "0" + date[2];
+			checkdate = "";
+			for (int i = 0; i < date.length; i++)
+				checkdate += date[i];
+		} else {
+			return null; // 위의 조건에 만족하지 않는 경우 없음
+		}
+		//return checkdate; // YYYYMMDD형식
+		
+		//의미규칙 확인 유효한 날짜인지
+		String s_year = checkdate.substring(0, 4);
+		String s_month = checkdate.substring(4,6);
+		if(s_month.startsWith("0"))
+			s_month = checkdate.substring(5,6);
+		String s_day = checkdate.substring(6, 8);
+		if(s_day.startsWith("0"))
+			s_day = checkdate.substring(7,8);
+		int year = Integer.parseInt(s_year);
+		int month = Integer.parseInt(s_month);
+		int day = Integer.parseInt(s_day);
+//		System.out.println(s_year);
+//		System.out.println(s_month);
+//		System.out.println(s_day);
+		boolean isLeapYear = false;
+		if((year / 4 == 0 && year / 100 != 0) || year / 400 == 0)
+			isLeapYear = true;
+		if(month>=1 && month <= 12) {
+			if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+				if(day >= 1 && day <= 31)
+					return checkdate;
+				else
+					return null;
+			}else if(month == 4 || month == 6 || month == 9 || month == 11) {
+				if(day >= 1 && day <= 30)
+					return checkdate;
+				else
+					return null;
+			}else if(month==2) {
+				if(isLeapYear) {
+					if(day >= 1 && day <= 29)
+						return checkdate;
+					else
+						return null;
+				}else {
+					if(day >= 1 && day <= 28)
+						return checkdate;
+					else
+						return null;
+				}
+			}else
+				return null;
+		}else
+			return null;
+	}
+	public static String Time2Rule() {// 7.7시각입력규칙 _ return 값은 HH:MM형식
+		String checktime = sc.nextLine();
+		boolean isNumeric = checktime.chars().allMatch(Character::isDigit);
+		if(isNumeric && checktime.length()==4) {
+			//매우 올바른형식임_아래 의미규칙만 확인
+		}else if(checktime.contains("-") || checktime.contains(":")) {
+			checktime = checktime.replace("-", ":"); // 구분기호 ":"로 통일
+			int count = 0;
+			for(int i =0; i<checktime.length(); i++)
+				if(checktime.charAt(i)==':')
+					count++;
+			if(count!=1)	return null;
+			String time[] = checktime.split(":");
+			if (time[0].length() == 1) {
+				time[0] = "0"+time[0];
+			}
+			if (time[1].length() == 1) {
+				time[1] = "0"+time[1];
+			}
+			checktime = "";
+			for (int i = 0; i < time.length; i++)
+				checktime += time[i];
+		} else {
+			return null;
+		}
+		checktime = checktime.substring(0, 2)+":"+checktime.substring(2, 4);
+		//return null;
+		//의미규칙확인
+		String s_hour = checktime.substring(0, 2);
+		String s_min = checktime.substring(3,5);
+		if(s_hour.startsWith("0"))
+			s_hour =  s_hour.substring(1,2);
+		int hour = Integer.parseInt(s_hour);
+		if(s_min.startsWith("0"))
+			s_min =  s_min.substring(1,2);
+		int min = Integer.parseInt(s_min);
+		if(hour>=0 && hour<24 && min>=0 && min < 60) {
+			return checktime;
+		}else
+			return null;
+	}
 	public static String SeatRule() { //7.9 예매 좌석 입력 규칙
         String seat = sc.nextLine();
         seat = seat.replace(" ", "");
