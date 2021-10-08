59b84c2b5d43c702e2484f007a41ac71bd9c1398
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2021-10-08 19:05:02 +0900

    commit to pull

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,5 @@ public class Main {
 	public static void main(String args[]) {
 		//main
 		//초기화면(로그인페이지) 호출
-		System.out.println("시간을 입력해주세요");
-		int time = InputRule.TimeRule();
-		System.out.println(time);
 	}
 }
