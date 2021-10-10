e35dd00d2bd53f439157d0d64b077ec68f5cf24e
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2021-10-11 04:10:38 +0900

    RunningInfo + info.js

--- src/RunningInfo.java
+++ src/RunningInfo.java
@@ -1,16 +1,26 @@
 
 
 public class RunningInfo {//상영정보
+	private String code;
 	private String date; //날짜, 시각, 상영관, 예약 좌석
 	private String time;
 	private String theater;
 	private ReserveInfo[] reserve;
 	
 	RunningInfo(String date, String time, String theater, ReserveInfo[] reserve){
+		this.code = date + time + theater;
 		this.date = date;
 		this.time = time;
 		this.theater = theater;
 		this.reserve = reserve;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
 	}
 
 	public String getDate() {
