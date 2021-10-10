1b14b22fbe2adb488829cafad486a339685527ff
rlawngP91 <91997710+rlawngP91@users.noreply.github.com>
2021-10-10 15:42:44 +0900

    TheaterInfo수정

---
+++ src/TheaterDataArray.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class TheaterDataArray {
+	public static ArrayList<TheaterInfo> theaterData = new ArrayList<>();
+	
+	//json에서 상영관정보읽어서 ArrayList의 TheaterInfo클래스에 입력
+	public static void readData() {
+		
+	}
+	
+}
