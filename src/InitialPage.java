a8c6589e23719c1b2f40fbdc0afe040bada65495
gurwls0122 <80228712+gurwls0122@users.noreply.github.com>
2021-10-23 23:30:03 +0900

    Main

--- src/InitialPage.java
+++ src/InitialPage.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class InitialPage {
    static Scanner scan = new Scanner(System.in);
-   public static void initialPage()
+   static UserInfo info;
+   public static UserInfo initialPage()
    {
 	     String[] menuname = {"","회원가입", "로그인"};
          while(true)
@@ -17,16 +18,23 @@ public class InitialPage {
             case 1:
                if(SignupPage.signupPage())
                {
-                  LoginPage.loginPage();
+                  info = LoginPage.loginPage();
+                  if(info!=null)
+                  {
+                	  return info;
+                  }
                }
-               continue;
-               
+               else
+            	   continue;
             case 2:
-               LoginPage.loginPage();
-               
+               info = LoginPage.loginPage();
+               if(info!=null)
+               {
+            	   return info;
+               }
             default:
             	System.out.println("올바르지 않은 입력입니다."); 
+         	}
          }
-      }
    }
 }
