b5b920b8a7f8b56f8a1992b63dfea9e33bb11857
최정윤 <99jenny0614@naver.com>
2021-10-23 12:10:32 +0900

    좌석 수정 페이지

--- src/ModifyRsrvSeatPage.java
+++ src/ModifyRsrvSeatPage.java
@@ -5,7 +5,8 @@ public class ModifyRsrvSeatPage{
 	private int[][]  theaterMap;
 	private int row;
 	private int col; 
-	
+	private int userSeatNum;
+	private int curSelectednum=0;
 	
 	public ModifyRsrvSeatPage(TheaterInfo theater, RunningInfo runInfo, ReserveInfo userRsrvInfo) {
 		
@@ -14,7 +15,35 @@ public class ModifyRsrvSeatPage{
 		int col = theater.getCol();
 		
 		theaterMap = new int[row][col]; // 0 : 빈 좌석, 1: 예매된 좌석 , 2: 현재 사용자 예매 좌석 
+		initTheaterMap(theater, runInfo, userRsrvInfo);
 		
+		userSeatNum=userRsrvInfo.getSeat().length;
+		
+	}
+
+	public void showPage() {
+		// 좌석표 출력
+		System.out.println("===== 예매 좌석 수정 =====");
+		printSeat(false);
+		System.out.println();
+		
+		while(true) {
+			String curSeatInfo = "["+ curSelectednum +"/"+userSeatNum+"]";
+			System.out.print("좌석을 선택해 주세요."+curSeatInfo+" (뒤로가기:0) >>>");
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+	}
+	
+	// theaterMap 초기화 함수 
+	private void initTheaterMap(TheaterInfo theater, RunningInfo runInfo, ReserveInfo userRsrvInfo) {
 		ArrayList<Pair> totalRsrvSeats = new ArrayList<Pair>(); // 전체 예매된 좌석 담는 배열
 		ArrayList<Pair> userRsrvSeats = new ArrayList<Pair>(); // 사용자가 현재 예매한 좌석 담는 배열 
 		
@@ -37,19 +66,11 @@ public class ModifyRsrvSeatPage{
 		for(Pair urs : userRsrvSeats) {
 			theaterMap[urs.getRow()][urs.getCol()] = 2;
 		}
-		
 	}
 
-
-	public void showPage() {
-		System.out.println("===== 예매 좌석 수정 =====");
-		
-		
-	}
-	
 	
 	// 현재 예매 좌석 제외하고 출력하기 - 마지막 인자 : 현재 예매 좌석 출력할건지 말건지 결정 
-	public void printSeat(TheaterInfo theater, RunningInfo runInfo, ReserveInfo userRsrvInfo, boolean includeCurSeat) {		
+	public void printSeat(boolean includeCurSeat) {		
 		// 출력 
 		System.out.println("□: 선택 가능 ▩: 예매 완료 ■: 현재 예매좌석");
 		// 첫번째 행
