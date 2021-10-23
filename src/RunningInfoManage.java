59b9c43441bdc60980fbcdeb545e533ea720682d
최정윤 <99jenny0614@naver.com>
2021-10-23 16:07:25 +0900

    예매 내역 삭제하는 메소드 추가

--- src/RunningInfoManage.java
+++ src/RunningInfoManage.java
@@ -77,7 +77,7 @@ public class RunningInfoManage {
         }
         return rt;
     }
-    
+    // 예매 내역 수정하는 함수 
     public static void modifyReserve(RunningInfo ri, ReserveInfo rsi, String id){
         getJson();
         try(FileWriter fw = new FileWriter(path)){
@@ -92,6 +92,36 @@ public class RunningInfoManage {
                     	// 해당 id 갖는 ReserveInfo 찾아서 내용변경 
                     	if(rsrvInfo.getUserId().equals(id)) 
                     		rsrvInfo = rsi;
+                    }
+                }
+            }
+            gson.toJson(riArr, fw);
+            fw.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // 예매 내역 삭제하는 함수 
+    public static void removeReserve(RunningInfo ri, ReserveInfo rsi, String id){
+        getJson();
+        try(FileWriter fw = new FileWriter(path)){
+            for(RunningInfo jsonRI : riArr){
+                if(ri.getMovieName().equals(jsonRI.getMovieName())
+                && ri.getTheater().equals(jsonRI.getTheater())
+                && ri.getTime().equals(jsonRI.getTime())
+                && ri.getDate().equals(jsonRI.getDate())){
+
+                	ArrayList<ReserveInfo> jsonReserve = jsonRI.getReserve();
+                    for(ReserveInfo rsrvInfo : jsonReserve) {
+                    	// 해당 id 갖는 ReserveInfo 찾아서 삭제 
+                    	if(rsrvInfo.getUserId().equals(id)) {
+                    		int rmIndex = jsonReserve.indexOf(rsrvInfo);
+                    		jsonReserve.remove(rmIndex);
+                    		break;
+                    	}
+                    		
                     }
                 }
             }
