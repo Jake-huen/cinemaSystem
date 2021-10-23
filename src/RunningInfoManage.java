6ca36f2158c39ac2ac3e3f8616e5271417d2fa8f
kkh <kkho9654@naver.com>
2021-10-23 18:42:07 +0900

    sejson2삭제

--- src/RunningInfoManage.java
+++ src/RunningInfoManage.java
@@ -60,20 +60,7 @@ public class RunningInfoManage {
             e.printStackTrace();
         }
     }
-    public static void setJson2(String date, String time, String movie, String theater, ArrayList<ReserveInfo> rsiArr) {
-        getJson();
-        try{
-        	FileWriter fw = new FileWriter(".\\resource\\info.json");
-            RunningInfo ri = new RunningInfo(date,time,movie,theater,rsiArr);
-            riArr.add(ri);
 
-            gson.toJson(riArr, fw);
-            fw.flush();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
     public static void updateReserve(RunningInfo ri, ReserveInfo rsi){
         getJson();
         try{
