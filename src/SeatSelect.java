a84c34a01457094a7cb55943762d1111c870627b
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2021-10-25 05:19:48 +0900

    Update SeatSelect.java

--- src/SeatSelect.java
+++ src/SeatSelect.java
@@ -28,27 +28,32 @@ public class SeatSelect {
         for(int i = 0; i < ppl; i++) {
             System.out.print("좌석을 선택해 주세요["+i+"/"+ppl+"]>>>");
             seatInput = InputRule.SeatRule();
-            int x = seatInput.charAt(0) - 'a';
-            int y = 0;
-            int result = 0;
-            if(seatInput.length() == 2) y = (seatInput.charAt(1) - '0') - 1;
-            else y = (seatInput.charAt(1)-'0') * 10 + (seatInput.charAt(2) - '0') - 1;
-
-            if(x < theaterX && y < theaterY){
-                if(seatArr[x][y]==1) result = -1;
-            }else{
-                result = -2;
-            }
-
-            if(result == 0){
-                rsSeat[i] = new Pair(x, y);
-            }else if(result == -1){
-                System.out.println("이미 선택된 좌석입니다.");
-                i--;
-            }
-            else{
+            if(seatInput == null) {
                 System.out.println("해당 좌석이 존재하지 않습니다.");
                 i--;
+            }
+            else {
+                int x = seatInput.charAt(0) - 'a';
+                int y = 0;
+                int result = 0;
+                if(seatInput.length() == 2) y = (seatInput.charAt(1) - '0') - 1;
+                else y = (seatInput.charAt(1)-'0') * 10 + (seatInput.charAt(2) - '0') - 1;
+
+                if(x < theaterX && y < theaterY){
+                    if(seatArr[x][y]==1) result = -1;
+                }else{
+                    result = -2;
+                }
+                if(result == 0){
+                    rsSeat[i] = new Pair(x, y);
+                }else if(result == -1){
+                    System.out.println("이미 선택된 좌석입니다.");
+                    i--;
+                }
+                else{
+                    System.out.println("해당 좌석이 존재하지 않습니다.");
+                    i--;
+                }
             }
         }
         seatReservedPrint(ri,rsSeat);
