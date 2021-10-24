df7a45f3d509744f00153448c6cbb34fa0e1f95e
gurwls0122 <80228712+gurwls0122@users.noreply.github.com>
2021-10-25 03:03:32 +0900

    merge

--- src/SearchMovie.java
+++ src/SearchMovie.java
@@ -49,42 +49,57 @@ public class SearchMovie {
                 if(date == null){
                     System.out.println("올바르지 않은 입력입니다.");
                 }else {
-                    break;
+                    ArrayList<RunningInfo> riList = new ArrayList<>();
+                    ArrayList<RunningInfo> riArr = RunningInfoManage.getRiArr();
+                    int count = 0;
+                    for(int i = 0; i < riArr.size(); i++) {
+                        if(riArr.get(i).getDate().equals(date))  {
+                            count++;
+                            riList.add(riArr.get(i));
+                        }
+                    }
+                    if(count == 0){
+                        System.out.println("해당 날짜에 상영되는 영화가 없습니다.");
+                        return;
+                    }
+
+                    int p = 1;
+                    for(RunningInfo ri : riList){
+                        System.out.println(p+". "+Print.makeDateFormet(ri.getDate())+" | "+Print.makeTimeFormet(ri.getTime())+" | "+ri.getTheater()+" | "+ri.getMovieName());
+                        p++;
+                    }
+                    System.out.print("예매할 영화를 선택해주세요(뒤로가기 : 0) >>>");
+                    int input = sc.nextInt();
+                    if(input == 0) {
+                        //날짜 입력 다시받기
+                        sc.nextLine();
+                        continue;
+                    }
+                    else {
+                        success(user, riList, input-1);
+                        return;
+                    }
                 }
             }
         }
-
-        ArrayList<RunningInfo> riList = new ArrayList<>();
-        ArrayList<RunningInfo> riArr = RunningInfoManage.getRiArr();
-        int count = 0;
-        for(int i = 0; i < riArr.size(); i++) {
-            if(riArr.get(i).getDate().equals(date))  {
-                count++;
-                riList.add(riArr.get(i));
-            }
-        }
-        if(count == 0){
-            System.out.println("해당 날짜에 상영되는 영화가 없습니다.");
-            return;
-        }
-
-        int p = 1;
-        for(RunningInfo ri : riList){
-            System.out.println(p+". "+Print.makeDateFormet(ri.getDate())+" | "+Print.makeTimeFormet(ri.getTime())+" | "+ri.getTheater()+" | "+ri.getMovieName());
-            p++;
-        }
-        System.out.print("예매할 영화를 선택해주세요(뒤로가기 : 0) >>>");
-        int input = sc.nextInt();
-        if(input == 0) return;
-        else success(user, riList, input-1);
     }
 
     public static void SearchByTitle(UserInfo user) {
         System.out.println("===== 영화검색_영화 제목 검색 모드 ======");
-        System.out.print("검색할 영화제목을 입력해주세요(뒤로가기: 0) >>> ");
-        String title=sc.nextLine(); //영화제목입력받기
-        String movieTitle = InputRule.MTRule(title);
-        if(Objects.isNull(title)) return;
+        String title = null;
+        String movieTitle = null;
+        while (true){
+            System.out.print("검색할 영화제목을 입력해주세요(뒤로가기: 0) >>> ");
+            title=sc.nextLine(); //영화제목입력받기
+            if(Objects.isNull(title)) {
+                System.out.println("올바르지 않은 입력입니다.");
+            }
+            else if(title.equals("0")) return;
+            else {
+                movieTitle = InputRule.MTRule(title);
+                break;
+            }
+        }
 
         ArrayList<RunningInfo> riList = new ArrayList<>();
         ArrayList<RunningInfo> riArr = RunningInfoManage.getRiArr();
@@ -96,6 +111,10 @@ public class SearchMovie {
             }
         }
 
+        if(riList.size() == 0){
+            System.out.println("검색하신 제목을 가진 영화가 없습니다.");
+            return;
+        }
         int p = 1;
         for(RunningInfo ri : riList){
             System.out.println(p+". "+Print.makeDateFormet(ri.getDate())+" | "+Print.makeTimeFormet(ri.getTime())+" | "+ri.getTheater()+" | "+ri.getMovieName());
