5e186f3593dec1747682450ebebb32fb89f5dbe1
최정윤 <99jenny0614@naver.com>
2021-10-26 02:06:52 +0900

    인코딩관련 코드추가

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
 
 public class Main {
 	static UserInfo userinfo;
 	public static void main(String args[]) {
+		System.setProperty("file.encoding", "UTF-8");
+		try {
+			Field charset = Charset.class.getDeclaredField("defaultCharset");
+			charset.setAccessible(true);
+			charset.set(null, null);
+		} catch (Exception e) {
+		}
+    
+		 
 		//SignupPage.signupPage();
 		userinfo = InitialPage.initialPage();
 		if(userinfo.getIsAdmin())
