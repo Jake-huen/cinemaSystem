51fe7f2ac41d3c713414721900276f5d105f426a
rlawngP91 <91997710+rlawngP91@users.noreply.github.com>
2021-12-08 21:29:02 +0900

    theaterdatamanage 다 바꿈

--- src/TheaterDataManage.java
+++ src/TheaterDataManage.java
@@ -125,7 +125,7 @@ public class TheaterDataManage {
 //		String[] str=getTheater("20210315","1000");
 //		System.out.println(str[0]+","+str[1]);
 //	}
-//	
+	
 	
 	// 상영관 정보 ArrayList로 받아오기 
 	public static ArrayList<TheaterInfo> getTheaterObjArr() { 
@@ -224,22 +224,27 @@ public class TheaterDataManage {
 			gson.toJson(temp,writer);
 			writer.flush();
 			writer.close();
-			System.out.println("잘됌");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	public static void main(String[] args) {
-		setJsonTheater("호경관",3,7,"20210315","1200");
-	}
+//	public static void main(String[] args) {
+//		setJsonTheater("호경관",3,7,"20210315","1200");
+//	}
 	
 	public static String readIndexTheater(int index) {//index해당하는 영화관 출력
-		JsonObject jsonobject = getJson();
-		JsonArray theaterInfos = (JsonArray)jsonobject.get("theaters");
-		String rt=((JsonObject) theaterInfos.get(index)).get("theater").toString();
-		int row=Integer.parseInt(((JsonObject) theaterInfos.get(index)).get("row").toString());
-		int col=Integer.parseInt(((JsonObject) theaterInfos.get(index)).get("col").toString());
+		JsonArray theaterInfos = getJson();
+		String rt=((JsonObject) theaterInfos.get(index)).get("name").toString();
+		rt =  Print.removeQuotes(rt);
+		LogData logdataNow = findTheater(rt, date, time);
+		
+		//여기 오류
+		if(logdataNow==null) {
+			System.out.println("에휴");
+		}
+		int row = logdataNow.getRow();
+		int col = logdataNow.getCol();
 		rt+="/"+row*col+"석";
 			//JsonObject movieinfo =(JsonObject)movieInfos.get(i);
 			//System.out.println(movieinfo.get("runtime"));
@@ -247,24 +252,23 @@ public class TheaterDataManage {
 	}
 	
 	public static String readIndexTheaterName(int index) {//index해당하는 영화관 출력
-		JsonObject jsonobject = getJson();
-		JsonArray theaterInfos = (JsonArray)jsonobject.get("theaters");
-		String rt=((JsonObject) theaterInfos.get(index)).get("theater").toString();
+		JsonArray theaterInfos = getJson();
+		String rt=((JsonObject) theaterInfos.get(index)).get("name").toString();
 			//JsonObject movieinfo =(JsonObject)movieInfos.get(i);
 			//System.out.println(movieinfo.get("runtime"));
 		return rt;
 	}
 	public static void fixTheater(int index,String newT,int row,int col) {//index받아와서 해당 영화관 수정
 		try {
-			Reader reader = new FileReader(pathTheater);
-			JsonParser jsonParser = new JsonParser();
-			JsonElement element = jsonParser.parse(reader);
-			JsonObject jsonobject = element.getAsJsonObject();
-			JsonArray theaterInfos = (JsonArray)jsonobject.get("theaters");
+			//Reader reader = new FileReader(pathTheater);
+			//JsonParser jsonParser = new JsonParser();
+			//JsonElement element = jsonParser.parse(reader);
+			//JsonObject jsonobject = element.getAsJsonObject();
+			JsonArray theaterInfos = getJson();
 			JsonObject theaterinfo =(JsonObject)theaterInfos.get(index);
 
 			//상영관이 수정된 날짜 확인한 다음 그 날짜에 해당되는 상영관 좌석의 행과 열 가져오기
-			String theaterName = ((JsonObject) theaterInfos.get(index)).get("theater").toString();
+			String theaterName = ((JsonObject) theaterInfos.get(index)).get("name").toString();
 			//int _row=Integer.parseInt(((JsonObject) theaterInfos.get(index)).get("row").toString());
 			//int _col=Integer.parseInt(((JsonObject) theaterInfos.get(index)).get("col").toString());
 			theaterName =  Print.removeQuotes(theaterName);
@@ -283,38 +287,33 @@ public class TheaterDataManage {
 					return;
 				}
 			}
+			//기존거 삭제되는지 확인해야댕
 			//2차기획서에 맞게 현재날짜와 함께 행 열 저장하기
-			LogData logdataNew = new LogData(date, time, row, col);
-			ArrayList<TheaterInfo> theaterInfoArr = getTheaterObjArr();
-			for(TheaterInfo t: theaterInfoArr) { 
-				if(t.getName().equals(theaterName)){	//이거 아닌듯
-					t.addLogData(logdataNew);
-				}
-			}
+			setJsonTheater(theaterName,row,col,date,time);
 			//기존의 theaterinfo에 상영관 이름이랑 log추가하기
 			
 			//theaterinfo.addProperty("theater", newT);
 			//theaterinfo.addProperty("row", row);
 			//theaterinfo.addProperty("col", col);
 			
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			String json = gson.toJson(element);
-			FileWriter writer=null;
-			try {
-				writer = new FileWriter(pathTheater);
-				writer.write(json);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}finally {
-				try {
-					writer.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		} catch (FileNotFoundException e) {
+//			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//			String json = gson.toJson(element);
+//			FileWriter writer=null;
+//			try {
+//				writer = new FileWriter(pathTheater);
+//				writer.write(json);
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}finally {
+//				try {
+//					writer.close();
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -326,7 +325,7 @@ public class TheaterDataManage {
 			JsonParser jsonParser = new JsonParser();
 			JsonElement element = jsonParser.parse(reader);
 			JsonObject jsonobject = element.getAsJsonObject();
-			JsonArray theaterInfos = (JsonArray)jsonobject.get("theaters");
+			JsonArray theaterInfos = getJson();
 			theaterInfos.remove(index);
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			String json = gson.toJson(element);
@@ -396,10 +395,12 @@ public class TheaterDataManage {
 				}
 			}
 		}
-		JsonObject jsonobject = getJson();
-		JsonArray theaterInfos = (JsonArray)jsonobject.get("theaters"); 
-		int row=Integer.parseInt(((JsonObject) theaterInfos.get(index)).get("row").toString());
-		int col=Integer.parseInt(((JsonObject) theaterInfos.get(index)).get("col").toString());
+		JsonArray theaterInfos = getJson(); 
+		LogData logdataNow = findTheater(theaterName, date, time);
+		int row = logdataNow.getRow();
+		int col = logdataNow.getCol();
+//		int row=Integer.parseInt(((JsonObject) theaterInfos.get(index)).get("row").toString());
+//		int col=Integer.parseInt(((JsonObject) theaterInfos.get(index)).get("col").toString());
 		return row*col;
 	}
 }
